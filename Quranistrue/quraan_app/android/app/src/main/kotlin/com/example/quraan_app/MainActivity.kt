cb628cb84a32826c473d7b75ed82083efe0feafc
Mohd Zaka Kareem <98280778+mdZakaDeveloper@users.noreply.github.com>
2023-09-13 03:43:27 +0530

    Delete Quranistrue directory

--- Quranistrue/quraan_app/android/app/src/main/kotlin/com/example/quraan_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quraan_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Quranistrue/quraan_app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-void main(){
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-   ),
-  );
-}
